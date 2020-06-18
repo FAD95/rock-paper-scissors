e9bd267feffdf8d0b153a7f8cead920983039c5c
Fadith Escorcia <feescorciam@protonmail.com>
2020-06-18 04:43:56 -0500

    PWA

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack')
 const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const TerserJSPlugin = require('terser-webpack-plugin')
+const OfflinePlugin = require('offline-plugin')
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/index.js'),
@@ -12,6 +13,7 @@ module.exports = {
     path: path.resolve(__dirname, 'docs'),
     chunkFilename: 'js/[id].[chunkhash].js',
     filename: 'js/[name].[hash].js',
+    // publicPath: path.resolve(__dirname, '/'),
   },
   optimization: {
     minimizer: [new TerserJSPlugin()],
@@ -37,9 +39,26 @@ module.exports = {
     ],
   },
   plugins: [
+    new OfflinePlugin({
+      relativePaths: true,
+      externals: [
+        '/index.html',
+        '/192.png',
+        '/512.png',
+        '/sw.js',
+        '/js/modules.3e518dd91d8bdcc6766c.dll.js',
+      ],
+      caches: 'all',
+      autoUpdate: true,
+      ServiceWorker: {
+        events: true,
+        prefetchRequest: { credentials: 'same-origin' },
+      },
+    }),
+    new webpack.EnvironmentPlugin({ ...process.env }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
-      favicon: path.resolve(__dirname, 'public/favicon.ico'),
+      favicon: path.resolve(__dirname, 'public/192.png'),
     }),
     new webpack.DllReferencePlugin({
       manifest: require('./modules-manifest.json'),
